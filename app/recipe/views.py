90d52898b2e2bcf31b0ad33da5b4bae49dc916b6
marimatush <marimatush@gmail.com>
2024-05-28 10:45:02 +0200

    Implement tag and ingredient filtering

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -104,7 +104,16 @@ class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
 
     def get_queryset(self):
         """Retrieve attribute queryset for authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
 
 
 class TagViewSet(BaseRecipeAttrViewSet):
